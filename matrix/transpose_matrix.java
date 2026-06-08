1996bb55aa64f1768e63fef0c976e9abeeb1c4aa
DHARANISH-M <dharanish0631@gmail.com>
2026-06-08 12:24:40 +0530

    transpose matrix

--- matrix/transpose_matrix.java
+++ matrix/transpose_matrix.java
@@ -1,22 +1,6 @@
 import java.util.*;
 
 public class Main {
-
-    public static int[][] transpose(int[][] matrix) {
-        int row = matrix.length;
-        int col = matrix[0].length;
-
-        int[][] arr = new int[col][row];
-
-        for (int i = 0; i < row; i++) {
-            for (int j = 0; j < col; j++) {
-                arr[j][i] = matrix[i][j];
-            }
-        }
-
-        return arr;
-    }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
@@ -31,11 +15,17 @@ public class Main {
             }
         }
 
-        int[][] result = transpose(matrix);
+        int[][] arr = new int[col][row];
 
-        for (int i = 0; i < result.length; i++) {
-            for (int j = 0; j < result[0].length; j++) {
-                System.out.print(result[i][j] + " ");
+        for (int i = 0; i < row; i++) {
+            for (int j = col - 1; j >= 0; j--) {
+                arr[j][row - 1 - i] = matrix[i][j];
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
             }
             System.out.println();
         }
